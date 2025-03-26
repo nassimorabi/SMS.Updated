0e8adb90b564232674b0c799be73b3115520879b
Jay Al Gallenero <jeegallenero@gmail.com>
2025-03-26 14:32:09 +0800

    added Programs

---
+++ SMS.Domain/Programs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Domain
+{
+    public class Programs
+    {
+        public int ProgramId { get; set; }
+        public string ProgramName { get; set; }
+        public string? Description { get; set; }
+    }
+}
